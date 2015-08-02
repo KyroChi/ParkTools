fc5dd484ac57692999a0fc43ec5aa238131e6403
Kyle Chickering <kylechickering@yahoo.com>
2015-08-02 17:33:56 -0600

    release 1.0.0

--- makerails.rb
+++ makerails.rb
@@ -1,8 +1,56 @@
+########################################################################################################################
+#                                                                                                                      #
+# ParkTools Plugin for SketchUp2015+                                                                                   #
+# Designed and built by:                                                                                               #
+# - Kyle Chickering                                                                                                    #
+# -                                                                                                                    #
+#                                                                                                                      #
+# With help from:                                                                                                      #
+# - Your Name Here                                                                                                     #
+#                                                                                                                      #
+# This plugin is free to use, if you want to modify this plugin feel free to submit changes to this                    #
+# plugin's GitHub page. You can do this by submitting a pull request or submitting a merge request with a              #
+# fork.                                                                                                                #
+#                                                                                                                      #
+# If you find any bugs please report them at the plugin's GitHub page by submitting an issue                           #
+#                                                                                                                      #
+# If there is a feature that you think this plugin should have feel free to submit an issue outlining the feature      #
+# and how it works in detail                                                                                           #
+#                                                                                                                      #
+# GitHub repository                                                                                                    #
+# https://github.com/KyroChi/ParkTools                                                                                 #
+#                                                                                                                      #
+# If you would like to talk to me about development or just Terrain Park design in general feel free to add            #
+# me on Skype @ 'KyleSesHello'                                                                                         #
+#                                                                                                                      #
+# Please don't send me issues that should be posted at GitHub, but for general development or talking shop             #
+# that is the best way to get a-hold of me.                                                                            #
+#                                                                                                                      #
+# Created in RubyMine by JetBrains on an Educational License                                                           #
+# ^^ Check out JetBrains and RubyMine, you will never go back!                                                         #
+#                                                                                                                      #
+########################################################################################################################
+
+
+
+
+
 require 'sketchup'
 
-UI.menu("Draw").add_item("Make new SUrail") {
 
-  prompts = ["Rail: Type",
+
+
+
+# Create menu option for generating rails
+UI.menu("Tools").add_item("Generate Rail") {
+
+
+
+
+
+  # All the prompts, defaults and lists for the input window
+  prompts = ["Raul: Name",
+             "Rail: Type",
              "Rail: Length",
              "Rail: Width",
              "Rail: Thickness",
@@ -17,110 +65,153 @@ UI.menu("Draw").add_item("Make new SUrail") {
              "Skirting: Width",
              "Skirting: Color"]
 
-  defaults = ["Bar", "15", "5", "2", "6", "1", "3", "2", "1", "1", "1", "1", "1","1"]
+  defaults = ["New Rail", "Bar", "15", "5", "2", "6", "1", "3", "2", "1", "1", "1", "8", "1","1"]
 
-  list = ["Tube|Bar|Handrail", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+  list = ["", "Tube|Bar|Handrail", "", "", "", "", "", "", "", "", "", "", "", "", ""]
 
-  input = UI.inputbox(prompts, defaults, list, "MakeRail")
+  # Create input window
+  input = UI.inputbox(prompts, defaults, list, "Park Tools Rail Generator")
 
-  if results = true
+  # If numbers are entered than create a rail
+  if true
 
     create_rail(*input)
 
   end
-
 }
 
-def create_rail(t, l, w, h, o, c, d, h2, s, d2, c2, h3, w2, c3)
 
-  rType       = t
-  rLength     = l.to_f * 12
-  rWidth      = w.to_f #* 12
-  rHeight     = h.to_f #* 12
-  rOver       = o.to_f
-  rColor      = c.to_f #* 12
-  sDiameter   = d.to_f #* 12
-  sHeight     = h2.to_f * 12
-  sSpan       = s.to_f #* 12
-  sDensity    = d2.to_f + 2#* 12
-  sColor      = c2.to_f #* 12
-  kHeight     = h3.to_f #* 12
-  kWidth      = w2.to_f #* 12
-  kColor      = c3.to_f #* 12
 
+
+
+# Main function that creates rails in the ParkTools toolkit
+# TODO break create_rail into seperate functions to improve devlopment
+def create_rail(n, t, l, w, h, o, c, d, h2, s, d2, c2, h3, w2, c3)
+
+
+
+
+
+  # Intialize entities for manipulation
   entities = Sketchup.active_model.active_entities
 
+
+
+
+
+  # Convert inputbox's output array into usable variables
+  r_name       = n.to_s
+  r_type       = t
+  r_length     = l.to_f * 12
+  r_width      = w.to_f #* 12
+  r_height     = h.to_f #* 12
+  r_over       = o.to_f
+  r_color      = c.to_f #* 12
+  s_diameter   = d.to_f #* 12
+  s_height     = h2.to_f * 12
+  s_span       = s.to_f #* 12
+  s_density    = d2.to_f + 2#* 12
+  s_color      = c2.to_f #* 12
+  k_height     = h3.to_f #* 12
+  k_width      = w2.to_f #* 12
+  k_color      = c3.to_f #* 12
+
+
+
+
+
   # Generate sliding surface if tube is chosen
-  #TODO Fix this mechanic
-  if rType == "Tube"
-    centerPoint = Geom::Point3d.new(rWidth, rWidth, 0)
+  # TODO Fix this mechanic
+  if r_type == "Tube"
+    center_point = Geom::Point3d.new(r_width, r_width, 0)
     vector = Geom::Vector3d.new 0, 1, 0
     vector2 = vector.normalize!
 
-    edges = entities.add_circle centerPoint, vector2, rWidth
+    edges = entities.add_circle center_point, vector2, r_width
   end
+
 
 
 
 
   # Generate sliding surface if bar is chosen
-  if rType == "Bar"
+  if r_type == "Bar"
 
-    pt1 = [0, 0, sHeight]
-    pt2 = [0, rLength, sHeight]
-    pt3 = [rWidth, rLength, sHeight]
-    pt4 = [rWidth, 0, sHeight]
+    pt1 = [0, 0, s_height]
+    pt2 = [0, r_length, s_height]
+    pt3 = [r_width, r_length, s_height]
+    pt4 = [r_width, 0, s_height]
 
     surface = entities.add_face pt1, pt2, pt3, pt4
-    surface.pushpull -rHeight
+    surface.pushpull -r_height
 
   end
+
 
 
 
 
   # Generate supports
-  supportIn = (rWidth - sDiameter) / 2
-  supportSpan = rLength - sDiameter - (2 * rOver)
-  supportGap = supportSpan / (sDensity - 1)
-  currentGap = 0.0
 
-  for support in 1..sDensity
+  # How far the outer edges of the support are from the outer edges of the surface
+  support_in = (r_width - s_diameter) / 2
+  # The distance from the centers of the two endcap supports
+  support_span = r_length - s_diameter - (2 * r_over)
+  # The distance between supports
+  support_gap = support_span / (s_density - 1)
+  # The current distance from the origin point, used in the for loop
+  current_gap = 0.0
 
-    pt1 = [supportIn, rOver + currentGap, 0]
-    pt2 = [supportIn, rOver + currentGap + sDiameter, 0]
-    pt3 = [supportIn + sDiameter, rOver + currentGap + sDiameter, 0]
-    pt4 = [supportIn + sDiameter, rOver + currentGap, 0]
+  # Creates all the required supports
+  for support in 1..s_density
 
+    # Creates a x, y coord cloud that represents the support footprint
+    pt1 = [support_in, r_over + current_gap, 0]
+    pt2 = [support_in, r_over + current_gap + s_diameter, 0]
+    pt3 = [support_in + s_diameter, r_over + current_gap + s_diameter, 0]
+    pt4 = [support_in + s_diameter, r_over + current_gap, 0]
+
+    # Creates and extrudes supports from x, y coord cloud
     support = entities.add_face pt1, pt2, pt3, pt4
-    support.pushpull -sHeight
+    support.pushpull -s_height
 
-    currentGap += supportGap
+    # Adds a gap so the next support is further down the rail
+    current_gap += support_gap
 
   end
+
 
 
 
 
   # Generate Skirting
-  skirtingIn = (rWidth - kWidth) / 2
-  skirtingSpan = rlength - (2 * rOver) - (sDiameter * sDensity)
-  skirtingGap = skirtingSpan / (sDensity - 1)
-  skirtingStart = rOver + sDiameter
 
-  for skirting in 1..sDensity
+  # How far the outer edge of the skirting is from the outer edge of the surface
+  skirting_in = (r_width - k_width) / 2
+  # How long each section of skirting is
+  skirting_span = (support_span / (s_density -1)) - s_diameter
+  # Origin point of the skirting
+  skirting_start = (r_over + s_diameter)
 
-    pt1 = [skirtingIn, skirtingStart, kHeight]
-    pt2 = [skirtingIn, skirtingStart + skirtingGap, kHeight]
-    pt3 = [skirtingIn + skirtingStart + skirtingGap, kHeight]
-    pt4 = [skirtingIn + kWidth, skirtingStart, kHeight]
+  # Creates required skirting sections
+  for skirting in 1..(s_density - 1)
 
+    # Creates a x, y coord cloud that represents the skirting footprint
+    pt1 = [skirting_in, skirting_start, k_height]
+    pt2 = [skirting_in, skirting_start + skirting_span, k_height]
+    pt3 = [skirting_in + k_width, skirting_start + skirting_span, k_height]
+    pt4 = [skirting_in + k_width, skirting_start, k_height]
+
+    # Creates and extrudes skirting from x, y coord cloud
     skirting = entities.add_face pt1, pt2, pt3, pt4
-    skirting.pushpull -(sHeight - kHeight)
+    skirting.pushpull -(s_height - k_height)
 
-    skirtingStart += skirtingGap + sDiameter
+    # Increases distange from origin so that skirting is located properly
+    skirting_start += skirting_span + s_diameter
 
   end
 
-  UI.messagebox("Debug")
+  # Turn on for debug or creation conformation
+  #UI.messagebox(r_name + " Created")
+
 end
